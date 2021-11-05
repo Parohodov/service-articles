6c4f747b513b230bf553f1bc3ec70f35fbd281d3
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-11-05 19:54:21 +0300

    Dependencies are added

--- src/main/java/ru/parohodov/servicearticles/ServiceArticlesApplication.java
+++ src/main/java/ru/parohodov/servicearticles/ServiceArticlesApplication.java
@@ -2,8 +2,10 @@ package ru.parohodov.servicearticles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ServiceArticlesApplication {
 
 	public static void main(String[] args) {
