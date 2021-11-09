f6af376b91ffa6dc1e10185c639f4e2889bbf8b2
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-11-09 20:45:13 +0300

    README.md file minor updates

--- setup-app.bat
+++ setup-app.bat
@@ -1,12 +1,10 @@
-mvnw.cmd clean install
-
 mkdir ArticleService\articles
 copy test-articles\gilmour.zip ArticleService\articles\
 copy test-articles\strat.zip ArticleService\articles\
 copy test-articles\drz.zip ArticleService\articles\
 
 mkdir ArticleService\test-articles
-xcopy test-articles ArticleService\test-articles\ \s
+xcopy test-articles ArticleService\test-articles\ /s
 
 copy target\servicearticles-0.0.1-SNAPSHOT.jar ArticleService
 copy src\main\resources\application.properties ArticleService
