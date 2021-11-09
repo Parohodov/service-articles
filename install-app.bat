f1cb7d848b1b71baa2652db8efad951d6dc3840f
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-11-09 20:15:17 +0300

    Bat file minor updates 2

--- install-app.bat
+++ install-app.bat
@@ -5,6 +5,7 @@ copy test-articles\gilmour.zip ArticleService\articles\
 copy test-articles\strat.zip ArticleService\articles\
 copy test-articles\drz.zip ArticleService\articles\
 
+mkdir ArticleService\test-articles
 xcopy test-articles ArticleService\test-articles\ \s
 
 copy target\servicearticles-0.0.1-SNAPSHOT.jar ArticleService
